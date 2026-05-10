def8037869b8c462c96e5741fb99028fa34a4d70
Husaynbek Muhammadyahyoyev <husayn@Husaynbeks-MacBook-Air.local>
2026-05-10 11:29:16 +0500

    fix contact views

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,5 @@
-import ssl
-import certifi
 from django.shortcuts import render
-from django.core.mail import get_connection, send_mail
+from django.core.mail import send_mail
 from django.conf import settings
 from .models import Contact
 
@@ -16,23 +14,11 @@ def contact_view(request):
         if name and email and message:
             Contact.objects.create(name=name, email=email, message=message)
 
-            ssl_context = ssl.create_default_context(cafile=certifi.where())
-
-            connection = get_connection(
-                host='smtp.gmail.com',
-                port=587,
-                username=settings.EMAIL_HOST_USER,
-                password=settings.EMAIL_HOST_PASSWORD,
-                use_tls=True,
-                ssl_context=ssl_context,
-            )
-
             send_mail(
                 subject=f"Yangi xabar: {name}",
                 message=f"Kimdan: {name}\nEmail: {email}\n\nXabar:\n{message}",
                 from_email=settings.EMAIL_HOST_USER,
                 recipient_list=['husaynsocial@gmail.com'],
-                connection=connection,
                 fail_silently=False,
             )
 
